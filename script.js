b4b5ff42e51090869cfbe6846c34f8a836b5f946
parsae80 <67233772+parsae80@users.noreply.github.com>
2021-03-01 15:01:01 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-    function compute()
-{
-    p = document.getElementById("principal").value;
 
+function compute() {
+    var principal = document.getElementById("principal").value;
+    if (principal <=0 ){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+    else {
+        var rate = document.getElementById("rate").value;
+        var years = parseInt(document.getElementById("years").value);
+        var interest = principal * years * rate / 100;
+
+
+        document.getElementById("result").innerHTML = `If you deposit <mark>${principal}</mark>,<br>
+    at an interest rate of <mark>${rate}%</mark>.<br>
+    You will receive an amount of <mark>${interest}</mark>,<br>
+    in the year <mark>${ parseInt(new Date().getFullYear()) +parseInt(years)}</mark>`
+    }
 }
+
+function rangeSlider() {
+    var slider = document.getElementById("slider").value;
+    document.getElementById("new").innerHTML = slider;
+}
+
+
+
